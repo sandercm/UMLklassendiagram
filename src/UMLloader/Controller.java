001231537581d10e7f535028e87e60f2af0bdf4a
Sander <sander.cm97@gmail.com>
2017-03-26 22:42:18 +0200

    fixed vbox placement

--- src/UMLloader/Controller.java
+++ src/UMLloader/Controller.java
@@ -41,11 +41,12 @@ public class Controller {
      * attributes no list of subclasses
      * relations no list of subclasses
      */
-    public void open(ActionEvent actionEvent){
+    public void open(ActionEvent actionEvent) throws FileNotFoundException{
         FileChooser chooser = new FileChooser();
         chooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("XML files", "*.xml"));
         File file = chooser.showOpenDialog(borderpane.getScene().getWindow());
         Unmarshal un = new Unmarshal();
+        //unmarshalling copypasta
         Diagram diagram = null;
         try {
             diagram = un.unmarshaller(file);
@@ -54,14 +55,32 @@ public class Controller {
         }
         setBoxes(diagram);
     }
-
+    /*TODO
+       * create the other objects and link them
+               * create the arrow
+               * create a border around the boxers
+               * create lines for the attributes
+     */
     private void setBoxes(Diagram diagram){
         for (int i = 0; i < diagram.getList().size(); i++){
             System.out.println(diagram.getList().get(i));
+            //creats Vbox objectr
             VBox newBox = new VBox();
-            Label label = new Label("my boxes bitch");
+            //creates lavel for the Vbox
+            Label label = new Label(diagram.getList().get(i).getName());
+            //adds the label to the Vbox
             newBox.getChildren().add(label);
+            newBox.setPrefWidth((double)diagram.getList().get(i).getWidth());
+            //String containing the css to add a border
+            //should find a better way to do this
+            String cssLayout = "-fx-border-color: black;\n" +
+                    "-fx-border-insets: 5;\n" +
+                    "-fx-border-width: 3;\n";
+            //links the css to the box+
+            newBox.setStyle(cssLayout);
+            //adds the box to the anchorpane
             anchorpane.getChildren().add(newBox);
+            //sets the location for the box on the plane
             AnchorPane.setTopAnchor(newBox, (double)diagram.getList().get(i).getRow());
             AnchorPane.setLeftAnchor(newBox, (double)diagram.getList().get(i).getCol());
         }
